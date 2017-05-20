b6cfb7cc583ebfb1ee6581c81f2803cc4604218e
AZName <964190389@qq.com>
2017-05-20 19:37:58 +0800

    第四天

---
+++ study/day4/rand om.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+"""
+    rand om.py
+Created by PythonStudy on 2017/5/20 下午7:28
+Copyright 2017 azhen All rights reserved.
+"""
+
+import random
+
+# print(random.random())
+
+# print(random.randrange(20))
+
+# print(random.randint(1, 3))
+
+# print(random.choice("azhen"))
+
+# print(random.sample("fsfsfsdf",2))
+print(random.sample("fsfsfsdf",2))
+
--- study/day4/time.py
+++ study/day4/time.py
@@ -8,4 +8,29 @@ Copyright 2017 azhen All rights reserved.
 
 import time
 
-print(time.time())
+import datetime
+# print(time.time())
+
+# print(time.sleep(1))
+
+# print(time.localtime())
+
+# print(time.asctime())
+
+# print(time.gmtime())
+
+# print(time.strftime("%Y--%m---%d"))
+
+# print(time.strftime("%Y--%m---%d %H:%M:%S", time.gmtime()))
+
+# print(datetime.datetime.now())
+# print(datetime.date.fromtimestamp(time.time()))
+
+# c_time = datetime.datetime.now()
+#
+# print(c_time.replace(minute=3,hour=2))
+
+# print(time.strftime("%j", time.gmtime()))
+
+# print(time.strftime("%x"))
+
