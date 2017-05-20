8e1e6f662e876df387deea955bd011015014d2d4
AZName <964190389@qq.com>
2017-05-21 02:12:46 +0800

    第五天

--- study/day4/os_modul.py
+++ study/day4/os_modul.py
@@ -5,3 +5,18 @@
 Created by PythonStudy on 2017/5/20 下午8:04
 Copyright 2017 azhen All rights reserved.
 """
+import os
+
+import sys
+
+# print(os.getcwd())
+
+# print(os.stat("."))
+
+# print(os.sep)
+# print(os.linesep)
+# print(os.pathsep)
+# print(os.environ)
+# print(os.path.join())
+
+print(sys.getdefaultencoding())
---
+++ study/day4/shutil_U.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# encoding: utf-8
+"""
+    shutil_U.py
+Created by PythonStudy on 2017/5/21 上午1:46
+Copyright 2017 azhen All rights reserved.
+"""
+import shutil
+
+
+print(shutil.copy())
