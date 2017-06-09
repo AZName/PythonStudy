dda49d6f33b8b310c838b9150035d4ca5af6454e
AZName <964190389@qq.com>
2017-06-09 15:45:23 +0800

    web 开发

--- study/day11/__init__.py
+++ study/day11/__init__.py
@@ -2,6 +2,6 @@
 # encoding: utf-8
 """
     __init__.py.py
-Created by PythonStudy on 2017/5/29 下午10:14
+Created by PythonStudy on 2017/5/29 下午10:day14
 Copyright 2017 azhen All rights reserved.
 """
