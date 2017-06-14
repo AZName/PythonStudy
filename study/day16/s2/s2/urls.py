bd2445260ac05366f3618cc521a7edcf44638b7c
AZName <964190389@qq.com>
2017-06-14 21:02:12 +0800

    web 开发

--- study/day16/s2/s2/urls.py
+++ study/day16/s2/s2/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 # from app1 import views
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^app/', include("app2.urls")),
 
 ]
